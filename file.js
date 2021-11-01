99307303ebf7e60923f46ff6a16a027a880bf375
Tatiana <tbolshakova@VRP.local>
2021-11-01 10:59:57 +0300

    This is commit

--- file.js
+++ file.js
@@ -1,5 +1,5 @@
-let money = prompt("Enter your monthly budget", 0),
- time = prompt("Enter data in YYYY-MM-DD format", '2021-10-29');
+let money = +prompt("Enter your monthly budget", ''),
+    time = prompt("Enter data in YYYY-MM-DD format", '2021-10-29');
 
 
 let appData = {
@@ -9,11 +9,34 @@ let appData = {
     optionalExpenses: {},
     income: [],
     savings: false
-} 
-
- 
-let q1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    q2 = prompt("Во сколько обойдется?")
+};
 
 
-alert(money / 30)
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?");
+
+        if( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
+        && a != '' && b != '' && a.length < 50) {
+            console.log("done");
+            appData.expenses[a] = b;
+        } else {
+
+        }
+      
+};
+
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Daily budget: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("Minimal salary");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Average salary");
+} else if(appData.moneyPerDay > 2000) {
+    console.log("High salary");
+} else {
+    console.log("Error!");
+}
